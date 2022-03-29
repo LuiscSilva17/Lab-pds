89e8a0974981582aa336fc30b74cecfe7261e7dd
HRPT2017 <83186577+HRPT2017@users.noreply.github.com>
2022-03-29 19:18:13 +0100

    Add files via upload

---
+++ Utad.Lab.3.Aula002/Utad.Lab.3.Ex2/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Utad.Lab._3.Ex2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BTGo_Click(object sender, RoutedEventArgs e)
+        {
+            string c=Convert.ToString(TBUrl.Text);
+            if(c == "side.utad.pt")
+            { 
+            Window browser = new Window();
+            this.Visibility = Visibility.Hidden;            
+            browser.Show();
+            }
+        }
+    }
+}
