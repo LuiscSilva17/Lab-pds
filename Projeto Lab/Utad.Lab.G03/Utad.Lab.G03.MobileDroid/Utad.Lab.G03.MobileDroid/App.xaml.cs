1314051fd9392a46008daa419a5dddb5c0f96054
LuiscSilva <93779347+LuiscSilva17@users.noreply.github.com>
2022-04-27 16:44:21 +0100

    Add files via upload

---
+++ Projeto Lab/Utad.Lab.G03/Utad.Lab.G03.MobileDroid/Utad.Lab.G03.MobileDroid/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Utad.Lab.G03.MobileDroid.Services;
+using Utad.Lab.G03.MobileDroid.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Utad.Lab.G03.MobileDroid
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Projeto Lab/Utad.Lab.G03/Utad.Lab.G03.MobileDroid/Utad.Lab.G03.MobileDroid/Models/Item.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Utad.Lab.G03.MobileDroid.Models
+{
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+    }
+}
